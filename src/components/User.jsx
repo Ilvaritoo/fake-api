192fb09985048483c4550788425f2f9ad552e1f9
Ilvars Grimza <ilvars.grimza@gmail.com>
2021-03-18 17:22:37 +0200

    start

---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = () => {
+    return (
+        <div className='user'>
+            Hello USER
+        </div>
+    )
+}
+
+export default User
---
+++ src/components/Users.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/main.css';
+import User from './User';
+
+const Users = () => {
+    return (
+        <>
+        <div className='users'>
+            <p className='users__text'>users</p>
+            
+        </div>
+        <User />
+        </>
+    )
+}
+
+export default Users
